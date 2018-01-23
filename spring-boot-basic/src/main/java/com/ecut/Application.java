75e88a7276f4b84b66b8736d15664a24187da6a9
songshuiyang <1459074711@qq.com>
2018-01-23 22:35:35 +0800

    新增 @ImportResource({"classpath:applicationContext.xml"})加载xml配置文件

--- spring-boot-basic/src/main/java/com/ecut/Application.java
+++ spring-boot-basic/src/main/java/com/ecut/Application.java
@@ -6,12 +6,14 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.web.WebApplicationInitializer;
 
 /**
  * springboot会自动加载启动类所在包下及其子包下的所有组件
  */
 @SpringBootApplication
+@ImportResource({"classpath:applicationContext.xml"})
 public class Application  extends SpringBootServletInitializer implements WebApplicationInitializer {
 
     /**
